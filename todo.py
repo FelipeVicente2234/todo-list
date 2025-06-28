caa11dc096732d7ad11c9d672e5602cd32da1ae3
Felipe Vicente <felipe.vicente@rede.ulbra.br>
2025-06-27 22:48:48 -0300

    Adiciona uma nova tarefa à lista com prioridade

--- todo.py
+++ todo.py
@@ -5,10 +5,10 @@ def adicionar_tarefa(tarefas, descricao):
     Uma tarefa é um dicionário com 'descricao' e 'concluida'.
     """
     if descricao:  # Garante que a descrição não está vazia
-        prioridade = input("Digite a prioridade (Alta, Média ou Baixa): ").capitalize()
-        if prioridade not in ["Alta", "Média", "Baixa"]:
-            print("⚠️ Prioridade inválida. Definindo como 'Baixa'.")
-            prioridade = "Baixa"
+       prioridade = input("Digite a prioridade (Alta, media, baixa):").capitalize()
+       if prioridade not in ["alta", "media", "baixa"]:
+        print("prioridade invalida. definindo como 'baixa'.")
+        prioridade = "baixa"
         nova_tarefa = {"descricao": descricao, "concluida": False, "prioridade" : prioridade}
         tarefas.append(nova_tarefa)
         print(f"\n✅ Tarefa '{descricao}' adicionada com sucesso!")
