77ea846f0a47979f243ea5f73f8882f69aed41a3
Felipe Vicente <felipe.vicente@rede.ulbra.br>
2025-06-27 23:32:36 -0300

    Criando nova opçao no menu

--- todo.py
+++ todo.py
@@ -76,6 +76,7 @@ def exibir_menu():
     print("2. Listar Tarefas")
     print("3. Marcar Tarefa como Concluída")
     print("4. Remover Tarefa")
+    print("5. Editar tarefa") #nova opçao
     print("0. Sair")
 
 def main():
